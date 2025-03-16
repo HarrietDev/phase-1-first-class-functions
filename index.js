451061d2c9f603e7a3b3c64e5b91b21dc7c40dd2
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-03-16 21:12:17 +0300

    first class functions test

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction (callback){
+    callback();
+}
+ function returnsANamedFunction (){
+    return function namedFunction(){
+ console.log('before all');
+};
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        console.log("before all");
+    };
+}
+ 
